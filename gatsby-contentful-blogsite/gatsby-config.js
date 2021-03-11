e4836584828fa4980f239304f9699e633f0f16c3
Your Nam <you@example.com>
2021-03-11 13:05:02 -0600

    Update the Contentful Post Update Issue

--- gatsby-contentful-blogsite/gatsby-config.js
+++ gatsby-contentful-blogsite/gatsby-config.js
@@ -38,6 +38,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        forceFullSync: true,
         plugins: [
           {
             resolve: `gatsby-remark-images-contentful`,
