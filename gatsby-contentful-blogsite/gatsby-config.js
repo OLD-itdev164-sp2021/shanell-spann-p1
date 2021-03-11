685ce997d92b7aa33ac7213af0807eb38ddcdaca
Your Nam <you@example.com>
2021-03-11 11:47:03 -0600

    Updated Gatsby-Config.js

--- gatsby-contentful-blogsite/gatsby-config.js
+++ gatsby-contentful-blogsite/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Blog Site`,
@@ -26,6 +30,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
