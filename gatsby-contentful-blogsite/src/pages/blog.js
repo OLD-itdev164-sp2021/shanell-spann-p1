d29ce87977da642059cea57b269a59a5a471be84
Your Nam <you@example.com>
2021-03-11 12:24:06 -0600

    Fixed Depreceated Gatsby Image Plugin

--- gatsby-contentful-blogsite/src/pages/blog.js
+++ gatsby-contentful-blogsite/src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
-import Img from "gatsby-image"
+import Img from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
